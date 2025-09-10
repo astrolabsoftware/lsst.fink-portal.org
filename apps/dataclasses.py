6d55d34917ffba56a327a8487faf83a772328094
JulienPeloton <peloton@lal.in2p3.fr>
2025-09-10 14:08:24 +0200

    Doc

--- apps/dataclasses.py
+++ apps/dataclasses.py
@@ -12,29 +12,33 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Definition of labels from TNS, SIMBAD and Fink"""
 from fink_utils.xmatch.simbad import get_simbad_labels
 
 import pandas as pd
 
+# TNS
 tns_types = pd.read_csv("assets/tns_types.csv", header=None)[0].to_numpy()
 tns_types = sorted(tns_types, key=lambda s: s.lower())
 
+# SIMBAD
 simbad_types = get_simbad_labels("old_and_new")
 simbad_types = sorted(simbad_types, key=lambda s: s.lower())
 
+# Fink
 fink_classes = [
-    "Anomaly",
+    # "Anomaly",
     "Unknown",
     # Fink derived classes
-    "Early SN Ia candidate",
-    "SN candidate",
-    "Kilonova candidate",
-    "Microlensing candidate",
-    "Solar System MPC",
-    "Solar System candidate",
-    "Tracklet",
-    "Ambiguous",
-    "(CTA) Blazar",
+    # "Early SN Ia candidate",
+    # "SN candidate",
+    # "Kilonova candidate",
+    # "Microlensing candidate",
+    # "Solar System MPC",
+    # "Solar System candidate",
+    # "Tracklet",
+    # "Ambiguous",
+    # "(CTA) Blazar",
     # TNS classified data
     *["(TNS) " + t for t in tns_types],
     # Simbad crossmatch
