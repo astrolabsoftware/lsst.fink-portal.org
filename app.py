fc9f55488343c2d39a97737c738bb099b103b0e0
JulienPeloton <peloton@lal.in2p3.fr>
2025-12-04 23:57:01 +0100

    Initial stat page

--- app.py
+++ app.py
@@ -15,12 +15,17 @@
 import dash
 import dash_mantine_components as dmc
 from dash import DiskcacheManager
+# import dash_auth
 
 import dash_bootstrap_components as dbc
 
 import os
 import diskcache
 
+USER_PWD = {
+    "username": "password",
+    "user2": "useSomethingMoreSecurePlease",
+}
 
 cache = diskcache.Cache("./cache")
 background_callback_manager = DiskcacheManager(cache)
@@ -56,6 +61,8 @@ app = factory(
     title="Fink/Rubin Science Portal",
     compress=True,
 )
+
+# auth = dash_auth.BasicAuth(app, USER_PWD, secret_key="toto")
 
 app.server.config["MAX_CONTENT_LENGTH"] = 100 * 1024 * 1024
 server = app.server
