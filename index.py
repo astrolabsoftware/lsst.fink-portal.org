15e6eb7c2f4a4f4091da6e3fa48a64bcf7812ab1
JulianHamo <hamo@ijclab.in2p3.fr>
2026-05-22 10:55:37 +0200

    Add broker external link (#107)
    Co-authored-by: Julian Hamo <julian.hamo@ijclab.in2p3.fr>

--- index.py
+++ index.py
@@ -107,6 +107,12 @@ navbar = html.Div(
                         make_navlink("Statistics", "ion:stats-chart-outline", "/stats"),
                         make_navlink("Schema", "ion:book-outline", "/schemas"),
                         make_navlink(
+                            "Broker",
+                            "ion:arrow-up-right-box-outline",
+                            "https://fink-broker.org/",
+                            target="_blank"
+                        ),
+                        make_navlink(
                             "Citing",
                             "ion:share-social",
                             "https://fink-broker.org/cite/",
