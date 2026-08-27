19da8a10a0debe45387672446127a96411830f37
Julien <peloton@lal.in2p3.fr>
2026-08-27 13:59:22 +0200

    Fix URL (#173)

--- apps/sso/cards.py
+++ apps/sso/cards.py
@@ -170,7 +170,7 @@ curl -H "Content-Type: application/json" -X POST \\
                                                 ),
                                                 download_tab,
                                                 dcc.Markdown(
-                                                    f"See {APIURL}/api for more options"
+                                                    f"See {APIURL} for more options"
                                                 ),
                                             ],
                                             "help_download_sso",
